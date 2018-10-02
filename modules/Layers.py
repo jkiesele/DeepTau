8e8583e07635db2ff2291ff1d61befd06263ec61
jkiesele <jkiesele@cern.ch>
2018-10-02 12:10:07 +0200

    cleanup

--- modules/Layers.py
+++ modules/Layers.py
@@ -10,6 +10,7 @@ import keras.backend as K
     
     
 class SortByFirstFeatureAndPool1D(Layer):
+    #this layer requires tensorflow >=1.8
     def __init__(self, noutputs , **kwargs):
         super(SortByFirstFeatureAndPool1D, self).__init__(**kwargs)
         self.noutputs=noutputs
@@ -39,30 +40,6 @@ class SortByFirstFeatureAndPool1D(Layer):
         _indexing_tensor = tf.concat([batch_range, I], axis=2)
        
         out = tf.gather_nd(inputs, _indexing_tensor)
-        print('get',out.shape)
-        return out
-    
-        #batchshape = tf.shape(inputs)[0].shape
-        #print(batchshape)
-        #if len(batchshape)>0:
-        #    n_batch=int(batchshape[0])
-        #params = inputs
-        #query = tf.nn.top_k(params[:, :, 0], k=self.noutputs, sorted=True).indices
-        #n = tf.shape(params)[0]
-        #ii = tf.tile(tf.range(n)[:, tf.newaxis, tf.newaxis], (1, self.noutputs, 1))
-        #idx = tf.concat([ii, query], axis=-1)
-        #result = tf.gather_nd(params, idx)
-        #print('get',result.shape)
-        #return result
-        #
-        batch_range = tf.expand_dims(tf.expand_dims(tf.range(n_batch), axis=1), axis=1)
-        batch_range = tf.tile(batch_range, [1,n_in, 1])
-        _indexing_tensor = tf.concat([batch_range, I], axis=2)
-        out = tf.gather_nd(inputs, _indexing_tensor)
-        
-        #somehow keras does not allow to use only the first k, so needs to be cut
-        #out = out[:,:self.noutputs]
-        print('get',out.shape)
         return out
     
 
